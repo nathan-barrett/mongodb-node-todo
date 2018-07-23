d700c31114ced0564d3652cb88c1d488738429fc
Nathan Barrett <demeules.barrett@gmail.com>
2018-07-23 09:34:02 -0700

    add hashing to all passwords, yay

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,12 +1,27 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-const data = {
-  id: 10
-};
+const password = '123abc';
 
-const token = jwt.sign(data, '123abc');
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
 
-const decoded = jwt.verify(token, '123abc');
+const hashedPassword = '$2a$10$WGxR9rgGd3mMVg5C2HAUSu8oi/eTQD8J2ZrrqKHra9ZM9oLfWRAgO';
 
-console.log('decoded *****', decoded);
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// const data = {
+//   id: 10
+// };
+
+// const token = jwt.sign(data, '123abc');
+
+// const decoded = jwt.verify(token, '123abc');
+
+// console.log('decoded *****', decoded);
